64618575781aa492f65cd514370851f7167000e1
slimsamil <samil-gencaslan@gmx.de>
2023-04-06 17:21:35 +0100

    added grades

--- models.go
+++ models.go
@@ -8,15 +8,6 @@ type Stringer interface{
 	String() string
 }
 
-func (s Student) String() string {
-	var str string = strconv.Itoa(s.matnumber) + ", " + s.name + ", " + s.address.String()
-	return str
-}
-
-func (a Address) String() string {
-	return a.straße + ", " + a.stadt
-}
-
 type Address struct {
 	straße string
 	stadt string
@@ -26,6 +17,7 @@ type Student struct {
 	matnumber int
 	name string
 	address Address
+	grades map[string]float32
 } 
 
 type SharedFlat struct{
@@ -33,7 +25,29 @@ type SharedFlat struct{
 	residents []Student
 }
 
+func (s Student) AddGrade(lecture string, grade float32) {
+	s.grades[lecture] = grade
+}
 
+func (s Student) GetOverallAverageGrade() float32 {
+	var average float32
+	var counter float32
+	for _, v := range s.grades {
+		average += v
+		counter++
+	}
+
+	return average / counter
+}
+
+func (s Student) String() string {
+	var str string = strconv.Itoa(s.matnumber) + ", " + s.name + ", " + s.address.String()
+	return str
+}
+
+func (a Address) String() string {
+	return a.straße + ", " + a.stadt
+}
 
 func (sf SharedFlat) AddStudent(student Student) {
 	sf.residents = append(sf.residents, student)
